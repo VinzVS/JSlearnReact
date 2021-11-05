66483abbe3cd31f8968774bfad63ec680e2624ed
Vinz <vinzvs@mail.ru>
2021-11-05 21:38:45 +0300

    first commit

---
+++ js/jsfile.js
@@ -0,0 +1,33 @@
+"use strict"; 
+
+console.log("hello");
+const number = 12;
+
+const obj = {
+    a: 2020,
+    name: 'John'
+};
+ 
+console.log(obj.a);
+
+let arr = ['plum','jell',1,2,3];
+console.log(arr[1]);
+
+//alert("hello");
+//const result = confirm('ТЫ тут ?');
+//console.log (result);
+//const answer = +prompt("Есть 18?","да");
+//console.log(typeof(answer));
+//console.log(answer)
+
+///const answers = [];
+//answers[0] = prompt("Как ваше имя","")
+//answers[1] = prompt("Как ваше Фамилия","");
+//answers[2] = +prompt("сколько лет","");
+//document.write(answers);
+//console.log(typeof(answers));
+
+let decr = 12;
+let acr = decr++;
+
+console.log(acr)
